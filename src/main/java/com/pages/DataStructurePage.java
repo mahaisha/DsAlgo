6fb6dd8834e188ba85b14b47dc7430c6f1f9a724
ShamiBapat <chaituvaidya@gmail.com>
2023-09-25 22:21:20 -0400

    Updated with camelcase naming conventions

--- src/main/java/com/pages/DataStructurePage.java
+++ src/main/java/com/pages/DataStructurePage.java
@@ -10,28 +10,26 @@ public class DataStructurePage {
 	
 	private WebDriver driver;
 	
-	private By DataStructureGetStarted = By.xpath("//a[@href ='data-structures-introduction']");
-	private By TimeComplexity_mainPage = By.linkText("Time Complexity");
-	private By Practice_questions = By.linkText("Practice Questions");
-	private By try_here_dataStructure = By.xpath("//a[@href='/tryEditor']");
-	private By runbutton = By.xpath("//button[contains(text(),'Run')]");
+	private By dataStructureGetStarted = By.xpath("//a[@href ='data-structures-introduction']");
+	private By timeComplexityMainPage = By.linkText("Time Complexity");
+	private By practicQuestions = By.linkText("Practice Questions");
 	
 	public DataStructurePage(WebDriver driver){
 		this.driver = driver;
 	}
 	
 	public void getStartedDataStructurePage() {	
-		driver.findElement(DataStructureGetStarted).click();		
+		driver.findElement(dataStructureGetStarted).click();		
 	}
 	
 	public String getDataStructureTitle() {
 		return driver.getTitle();
 	}
 	public void clickTimeComplexityLink(){
-		driver.findElement(TimeComplexity_mainPage).click();
+		driver.findElement(timeComplexityMainPage).click();
 	}
 	public void clickPracticeQuestions() {
-		driver.findElement(Practice_questions).click();
+		driver.findElement(practicQuestions).click();
 	}
 	
 }
