9549569ffab0c169f384fceb840ef54b92d27cb2
MeenakshiMuthuraman <144947917+MeenakshiMuthuraman@users.noreply.github.com>
2023-10-17 22:16:34 -0400

    added plugins in runner file

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -4,12 +4,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 
 @io.cucumber.testng.CucumberOptions(
-		features= {"src/test/resources/AppFeatures"},
+		features= {"src/test/resources/AppFeatures/AlandingPage.feature"},
 		glue= {"stepdefinition","AppHooks"},
-		plugin= {"pretty",
+		plugin= {"pretty","html:target/cucumber-reports/reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"json:target/cucumber.json", 
-				"rerun:target/rerun.txt"}
+				"json:target/cucumber-reports/cucumber.json",
+				"junit:target/cucumber-reports/cucumber.xml",
+				"rerun:target/rerun.txt"},
+		monochrome = true
 		
 		)
 
