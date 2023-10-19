d9ad7fb0e20d8f855e14a6af3de06bbc2319cfec
MeenakshiMuthuraman <144947917+MeenakshiMuthuraman@users.noreply.github.com>
2023-10-19 14:46:46 -0400

    "rerun check with two runner file"

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 
 @io.cucumber.testng.CucumberOptions(
-		features= {"src/test/resources/AppFeatures"},
+		features= {"src/test/resources/AppFeatures/Blogin.feature","src/test/resources/AppFeatures/graphPage.feature"},
 		glue= {"stepdefinition","AppHooks"},
 		plugin= {"pretty","html:target/cucumber-reports/reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
