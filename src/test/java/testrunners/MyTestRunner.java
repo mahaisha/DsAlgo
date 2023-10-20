85fd0532ec4cdd73836dafe8e57a9ab0911804ab
MeenakshiMuthuraman <144947917+MeenakshiMuthuraman@users.noreply.github.com>
2023-10-20 13:01:41 -0400

    "Final code to push to main branch"

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 
 @io.cucumber.testng.CucumberOptions(
-		features= {"src/test/resources/AppFeatures/Blogin.feature","src/test/resources/AppFeatures/graphPage.feature"},
+		features= {"src/test/resources/AppFeatures"},
 		glue= {"stepdefinition","AppHooks"},
 		plugin= {"pretty","html:target/cucumber-reports/reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
